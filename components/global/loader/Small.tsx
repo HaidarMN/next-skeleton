a03bc9bf0d94a16a58e2322a2fd8f0ba3b2daab5
saturniesm <saturniesm@gmail.com>
2024-03-07 11:53:45 +0700

    feat(button): add small spinner

---
+++ components/global/loader/Small.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="text-center">
+      <div className=" h-6 w-6 animate-spin rounded-full border-4 border-gray-300 border-t-slate-950"></div>
+    </div>
+  );
+};
+
+export default Loader;
